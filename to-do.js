a9d08305b8066724db677f2b6a8197057edc963b
Paulin <shemampa2010@gmail.com>
2026-07-17 17:46:56 +0200

    made the checkbox work

--- to-do.js
+++ to-do.js
@@ -1,32 +1,58 @@
 const tasks = [];
 
+
 function addTask() {
 
     let input = document.getElementById("taskInput");
 
     if (input.value === "") return;
 
+
     tasks.push({
         texte: input.value,
         done: false
     });
 
-    let index = tasks.length - 1;
 
     let li = document.createElement("li");
     li.className = "task";
+
 
     li.innerHTML =
     "<input type='checkbox'>" +
     "<span>" + input.value + "</span>" +
     "<button class='sup' onclick='deleteTask(this)'>Supprimer</button>";
 
+    let checkbox = li.querySelector("input");
+
+    let task = checkbox.parentElement;
+
+    checkbox.addEventListener("change", function () {
+
+        console.log("one");
+
+        if (checkbox.checked) {
+
+            task.style.textDecoration = "line-through";
+            task.style.color = "gray";
+
+        } else {
+
+            task.style.textDecoration = "none";
+            task.style.color = "black";
+
+        }
+
+    });
+
+
     document.getElementById("list").appendChild(li);
+
 
     input.value = "";
 
-    console.log(tasks);
 }
+
 
 
 function deleteTask(button) {
@@ -38,38 +64,28 @@ function deleteTask(button) {
 }
 
 
-function checkTask(checkbox) {
-
-    let task = checkbox.parentElement;
-
-    if (checkbox.checked) {
-
-        task.style.textDecoration = "line-through";
-        task.style.color = "gray";
-
-    } else {
-
-        task.style.textDecoration = "none";
-        task.style.color = "black";
-
-    }
-}
-
 
 function deleteAll() {
 
     let allTasks = document.querySelectorAll("#list li");
+
 
     allTasks.forEach(function(task) {
 
         task.style.textDecoration = "line-through";
         task.style.color = "gray";
 
+
         setTimeout(function() {
+
             task.remove();
+
         }, 1300);
+
 
     });
 
+
     tasks.length = 0;
+
 }
